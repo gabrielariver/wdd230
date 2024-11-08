9330f6ad023681978b91344d78b2c34b8a423113
gabrielariver <riv23011@byui.edu>
2024-11-08 03:21:20 -0500

    Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+
+//current year js
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+//last modification
+document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
