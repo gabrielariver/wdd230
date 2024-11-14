92a80276fad426c1f6e5a50a0fcc2bc73faaa980
gabrielariver <riv23011@byui.edu>
2024-11-14 01:47:41 -0500

    Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,24 @@
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 //last modification
 document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
+
+//Hamburger 
+const menuButton = document.getElementById('menuButton');
+const mainNav = document.getElementById('mainNav');
+
+menuButton.addEventListener('click', () => {
+    mainNav.querySelector('ul').classList.toggle('active');
+    menuButton.textContent = menuButton.textContent === '☰' ? '✕' : '☰';
+});
+
+// Dark Button
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.innerHTML = '☀️ Light Mode';
+        darkModeToggle.innerHTML = '🌙 Dark Mode';
+    }
+});
