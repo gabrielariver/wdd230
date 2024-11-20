ebc3270c295b6458b218fa41d9a19e8837b3e10c
gabrielariver <riv23011@byui.edu>
2024-11-20 10:15:04 -0500

    Lazy Load Example

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,6 @@
+
+//last modification
+document.addEventListener("DOMContentLoaded", function () {
+    const lastModified = document.lastModified;
+    document.getElementById("last-modified").textContent = lastModified;
+});
