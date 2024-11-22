0f53aa8f939d111c73ba03353ed249c63995f790
gabrielariver <riv23011@byui.edu>
2024-11-22 12:11:56 -0500

    The Chamber Discover Page Week03

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -15,3 +15,24 @@ document.addEventListener('DOMContentLoaded', function () {
         navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
     });
 });
+//last visit
+const lastVisitContainer = document.querySelector(".sidebar");
+
+const currentDate = Date.now();
+
+const lastVisit = localStorage.getItem("lastVisit");
+
+if (!lastVisit) {
+    lastVisitContainer.innerHTML += `<p>Welcome! Let us know if you have any questions.</p>`;
+} else {
+    const daysDifference = Math.floor((currentDate - lastVisit) / (1000 * 60 * 60 * 24));
+    if (daysDifference < 1) {
+        lastVisitContainer.innerHTML += `<p>Back so soon! Awesome!</p>`;
+    } else if (daysDifference === 1) {
+        lastVisitContainer.innerHTML += `<p>You last visited 1 day ago.</p>`;
+    } else {
+        lastVisitContainer.innerHTML += `<p>You last visited ${daysDifference} days ago.</p>`;
+    }
+}
+
+localStorage.setItem("lastVisit", currentDate);
